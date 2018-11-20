b6305a72290621870233a0b0d23acd82574d3f52
Dmitry.Shuplyakov <dmitry1405@gmail.com>
2018-11-20 22:18:36 +0300

    DoubleBarrier new version

--- src/main/java/com/dshuplyakov/recipes/DoubleBarier.java
+++ src/main/java/com/dshuplyakov/recipes/DoubleBarier.java
@@ -3,6 +3,11 @@ package com.dshuplyakov.recipes;
 import com.dshuplyakov.ZookeeperInit;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.barriers.DistributedDoubleBarrier;
+import org.apache.curator.utils.ZKPaths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Date: 13.10.2018
@@ -14,23 +19,31 @@ import org.apache.curator.framework.recipes.barriers.DistributedDoubleBarrier;
 //java -cp zk-jar-with-dependencies.jar com.dshuplyakov.recipe.DoubleBarier 1
 public class DoubleBarier {
 
-    private static final int QTY = 5;
+    private static final int QTY = 2;
     private static final String PATH = "/examples/barrier";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DoubleBarier.class);
+
     public static void main(String[] args) throws InterruptedException {
-        String clientId = args[0];
         CuratorFramework curatorFramework = ZookeeperInit.getCuratorFrameworkClient();
         final DistributedDoubleBarrier barrier = new DistributedDoubleBarrier(curatorFramework, PATH, QTY);
         try {
-            Thread.sleep((long) (30000 * Math.random()));
-            System.out.println("Client #" + clientId + " wait before enter...");
-            barrier.enter();
-            System.out.println("Client #" + clientId + " entered! Doing some work... and wait until others client do");
-            Thread.sleep((long) (30000 * Math.random()));
-            barrier.leave();
-            System.out.println("Client #" + clientId + " left");
+            LOGGER.info("Client # wait before enter...");
+            barrier.enter(30, TimeUnit.SECONDS);
+            LOGGER.info("Doing some work... ");
+            //читаем значение в локальную переменую.
+
+            if (args.length>0) {
+                System.out.println("exit");
+                Thread.sleep(Long.MAX_VALUE);
+            }
+
+            Thread.sleep(7000);
+            LOGGER.info("wait until others client finished");
+            boolean result = barrier.leave(3, TimeUnit.SECONDS);
+            LOGGER.info("Client #  left {}", result);
         } catch (Exception e) {
-            System.out.println(e);
+            LOGGER.info(e.getMessage(), e);
         }
     }
 
